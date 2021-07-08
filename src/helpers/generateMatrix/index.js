727de01b2dfa3ec478c34301ddfdbed2f5377e5a
putrasatr <satriaputrapratama2002@gmail.com>
2021-07-08 19:38:08 +0700

    Create Helper Email Validator

---
+++ src/helpers/generateMatrix/index.js
@@ -0,0 +1,35 @@
+export default function generateMatrix(activeDate,weekDays,nDays) {
+    let matrix = [];
+    // Create header
+    matrix[0] = weekDays;
+
+    // More code here
+    const year = activeDate.getFullYear();
+    const month = activeDate.getMonth();
+
+    const firstDay = new Date(year, month, 1).getDay();
+    let maxDays = nDays[month];
+    if (month == 1) { // February
+      if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+        maxDays += 1;
+      }
+    }
+
+    let counter = 1;
+    for (let row = 1; row < 7; row++) {
+      matrix[row] = [];
+      for (let col = 0; col < 7; col++) {
+        matrix[row][col] = -1;
+        if (row == 1 && col >= firstDay) {
+          // Fill in rows only after the first day of the month
+          matrix[row][col] = counter++;
+        } else if (row > 1 && counter <= maxDays) {
+          // Fill in rows only if the counter's not greater than
+          // the number of days in the month
+          matrix[row][col] = counter++;
+        }
+      }
+    }
+
+    return matrix;
+  }
